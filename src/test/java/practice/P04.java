80c141d40d898e905f47433e1c2f590be5357c67
Adem Yusuf <adiguzelademyusuf@gmail.com>
2022-12-27 23:58:10 +0300

    12/27/2022

---
+++ src/test/java/practice/P04.java
@@ -0,0 +1,62 @@
+package practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class P04 {
+    static WebDriver driver;
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @AfterClass
+    public static void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.close();
+    }
+    // http://the-internet.herokuapp.com/add_remove_elements/ adresine gidiniz
+    // Add Element butonuna 100 defa basınız
+    // 100 defa basıldığını test ediniz
+    // 90 defa delete butonuna basınız
+    // 90 defa basıldığını doğrulayınız
+    // Sayfayı kapatınız
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement elemetnsButton = driver.findElement(By.cssSelector("button[onclick='addElement()']"));
+        for (int i = 0; i < 100; i++) {
+            elemetnsButton.click();
+        }
+
+
+        Thread.sleep(3000);
+
+    }
+    @Test
+    public void test02() {
+        List<WebElement> listDelete = driver.findElements(By.xpath("//button[text()='Delete']"));
+
+        Assert.assertEquals(100, listDelete.size());
+
+        int count = 0;
+        for (WebElement w : listDelete) {
+            w.click();
+            count++;
+            if (count == 90) {
+                break;
+            }
+        }
+
+        Assert.assertEquals(90, count);
+    }
+}
---
+++ src/test/java/practice/P05.java
@@ -0,0 +1,59 @@
+package practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.security.Key;
+import java.time.Duration;
+
+public class P05 {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @Before
+    public void setUp2() {
+        driver.navigate().to("http://www.google.com");
+    }
+    //BeforeClass ile driver ı olusturun ve
+    //Maximize edin, 15 sn bekletin
+    //http://www.google.com adresine gidin
+    //arama kutusuna "The God Father" yazip, cikan sonuc sayisini yazdirin
+    //arama kutusuna  "Lord of the Rings" yazip, cikan sonuc sayisini yazdirin
+    //arama kutusuna  "Kill Bill" yazip, cikan sonuc sayisini yazdirin
+    //AfterClass ile kapatın
+    @Test
+    public void test01() throws InterruptedException {
+        driver.findElement(By.cssSelector("input[class='gLFyf']")).sendKeys("The God Father", Keys.ENTER);
+        Thread.sleep(3000);
+    }
+    @Test
+    public void test02() throws InterruptedException {
+        driver.findElement(By.cssSelector("input[class='gLFyf']")).sendKeys( "Lord of the Rings", Keys.ENTER);
+        Thread.sleep(3000);
+    }
+    @Test
+    public void test03() throws InterruptedException {
+        driver.findElement(By.cssSelector("input[class='gLFyf']")).sendKeys( "Kill Bill", Keys.ENTER);
+    }
+    @After
+    public void tearDown2() throws InterruptedException {
+        String [] findedresult1 = driver.findElement(By.cssSelector("div[id='result-stats']")).getText().split(" ");
+        System.out.println("Arama sonucu = "+ findedresult1[1]);
+    }
+    @AfterClass
+    public static void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.close();
+    }
+}
