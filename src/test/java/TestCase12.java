dbe6043030e4a230ded52c018ea124dc40f98cea
Adem Yusuf <adiguzelademyusuf@gmail.com>
2023-01-14 21:19:03 +0300

    01/14/2023

--- src/test/java/TestCase12.java
+++ src/test/java/TestCase12.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 public class TestCase12 extends TestBase {
@@ -19,12 +22,42 @@ public class TestCase12 extends TestBase {
     @Test
     public void test1() {
         driver.get("https://automationexercise.com");
+        Actions actions = new Actions(driver);
 
         Assert.assertEquals("https://automationexercise.com/", driver.getCurrentUrl());
-        driver.findElement(By.xpath("//a[.=' Cart']")).click();
-
         driver.findElement(By.xpath("//i[@class='material-icons card_travel']")).click();
+        driver.navigate().back();
+        driver.navigate().forward();
+        waitFor(1);
 
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        waitFor(1);
+
+
+        WebElement fistProductimg = driver.findElement(By.xpath("//img[@src='/get_product_picture/1']"));
+        actions.moveToElement(fistProductimg).click(driver.findElement(By.xpath("(//a[@data-product-id='1'])[2]"))).perform();
+        String firstProductPrice = driver.findElement(By.xpath("(//div[@class='single-products']//h2)[1]")).getText();
+        waitFor(1);
+        driver.findElement(By.xpath("//button[@data-dismiss='modal']")).click();
+
+        WebElement secondProduct = driver.findElement(By.xpath("(//a[@data-product-id='1'])[2]"));
+        WebElement secondProductimg = driver.findElement(By.xpath("//img[@src='/get_product_picture/2']"));
+        String secondProductPrice = driver.findElement(By.xpath("(//div[@class='single-products']//h2)[3]")).getText();
+        secondProduct.click();
+        waitFor(1);
+        driver.findElement(By.xpath("//p[@class='text-center']//u")).click();
+
+        WebElement bothProduct1 = driver.findElement(By.xpath("//img[@src='get_product_picture/1']"));
+        WebElement bothProduct2 = driver.findElement(By.xpath("//img[@src='get_product_picture/2']"));
+
+        Assert.assertEquals(fistProductimg, bothProduct1);
+        Assert.assertEquals(secondProductimg, bothProduct2);
+
+        String price1 = driver.findElement(By.xpath("(//p[@class='cart_total_price'])[1]")).getText();
+        String price2 = driver.findElement(By.xpath("(//p[@class='cart_total_price'])[1]")).getText();
+
+        Assert.assertEquals(firstProductPrice, price1);
+        Assert.assertEquals(secondProductPrice, price2);
 
     }
 
